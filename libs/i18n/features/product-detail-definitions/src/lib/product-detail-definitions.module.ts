6323bb649a0428eced8677f1981fa99ef7696fb2
Caroline Chaibainou <chaibainou@kizeo.com>
2021-12-27 09:52:40 +0100

    Feat/definitions crud

--- libs/i18n/features/product-detail-definitions/src/lib/product-detail-definitions.module.ts
+++ libs/i18n/features/product-detail-definitions/src/lib/product-detail-definitions.module.ts
@@ -2,6 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductDetailDefinitionsComponent } from './product-detail-definitions.component';
 import { RouterModule } from '@angular/router';
+import { NzTableModule } from 'ng-zorro-antd/table';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { FormsModule } from '@angular/forms';
+import { NzInputModule } from 'ng-zorro-antd/input';
 
 @NgModule({
   declarations: [ProductDetailDefinitionsComponent],
@@ -9,7 +13,11 @@ import { RouterModule } from '@angular/router';
     CommonModule,
     RouterModule.forChild([
       { path: '', component: ProductDetailDefinitionsComponent }
-    ])
+    ]),
+    NzTableModule,
+    NzFormModule,
+    NzInputModule,
+    FormsModule,
   ],
 })
 export class ProductDetailDefinitionsModule {}
