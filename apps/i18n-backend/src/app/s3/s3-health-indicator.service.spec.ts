93ca72cc00dd65aa620ac24d1f481ce168b683d7
Clément D <delhamaide@kizeo.com>
2022-02-10 17:43:32 +0100

    Fix spec file

--- apps/i18n-backend/src/app/s3/s3-health-indicator.service.spec.ts
+++ apps/i18n-backend/src/app/s3/s3-health-indicator.service.spec.ts
@@ -1,15 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { S3HealthIndicatorService } from './s3-health-indicator.service';
+import { S3HealthIndicator } from './s3-health-indicator.service';
 
-describe('S3HealthIndicatorService', () => {
-  let service: S3HealthIndicatorService;
+describe('S3HealthIndicator', () => {
+  let service: S3HealthIndicator;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [S3HealthIndicatorService],
+      providers: [S3HealthIndicator],
     }).compile();
 
-    service = module.get<S3HealthIndicatorService>(S3HealthIndicatorService);
+    service = module.get<S3HealthIndicator>(S3HealthIndicator);
   });
 
   it('should be defined', () => {
